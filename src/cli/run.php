ff9847aa4bacc91cc2ecaadc89a4dbf96aa856c5
nabab <thomas.nabet@gmail.com>
2022-04-03 16:51:34 +0200

    Added options config

--- src/cli/run.php
+++ src/cli/run.php
@@ -1,8 +1,10 @@
 <?php
+use bbn\Appui\Statistic;
+
 /** @var \bbn\mvc\controller $ctrl */
-foreach (\bbn\appui\statistic::get_options('active') as $o) {
-  if ($cfg = $ctrl->get_plugin_model($o['code'], [], $ctrl->plugin_url('appui-statistics'))) {
-    $stat = new \bbn\appui\statistic($ctrl->db, $o['code'], $cfg);
+foreach (Statistic::getOptions('active') as $o) {
+  if ($cfg = $ctrl->getPluginModel($o['code'], [], $ctrl->pluginUrl('appui-statistic'))) {
+    $stat = new Statistic($ctrl->db, $o['code'], $cfg);
     $stat->update();
   }
 }
